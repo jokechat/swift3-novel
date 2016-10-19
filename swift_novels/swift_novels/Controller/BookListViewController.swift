434797c485d564f7a57f5253c579417814231532
jokechat <jokechat@qq.com>
2016-10-19 16:59:40 +0800

    tableview 动态填充数据完成 下一步计划 1 view跳转传递参数 2 网络图片下载以及展示 3 网络请求获取小说json数据 4 添加返回按钮 返回上一次view,非跳转到指定view

--- swift_novels/swift_novels/Controller/BookListViewController.swift
+++ swift_novels/swift_novels/Controller/BookListViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class BookListViewController: UITableViewController {
 
+    let myData  = ["水浒传","三国演义","西游记","红楼梦","本草纲目","中华上下五千年"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,15 +25,47 @@ class BookListViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func back()
+    {
+        print("点击了返回按钮")
+        
+        let listViewController = self.storyboard?.instantiateViewController(withIdentifier: "manView")
+        self.show(listViewController!, sender: nil)
+        
+        
     }
-    */
 
+    // table view 节点数量
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    // 动态table view 中单元格数量
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myData.count
+    }
+    
+    // tableview 中单元格填充数据
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "reuseIdentifier")
+        
+        let index   = ((indexPath as NSIndexPath).row)
+        
+        cell.imageView?.image = UIImage(named:"hhh.png")
+        cell.textLabel?.text = "\(myData[index])"
+        cell.detailTextLabel?.text = "详情lab"
+        return cell
+    }
+    
+    // tableview 选取行
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let cellView = tableView.cellForRow(at: indexPath)
+        // 单元格文本数据
+//        cellView?.textLabel?.text
+        
+        let listViewController = self.storyboard?.instantiateViewController(withIdentifier: "manView")
+        self.show(listViewController!, sender: nil)
+    }
+    
 }
